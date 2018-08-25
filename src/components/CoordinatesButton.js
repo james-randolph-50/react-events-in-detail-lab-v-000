3036d7bf0e6792cec18bb66890be0552fc7234e9
James Randolph <james.randolph2016@gmail.com>
2018-08-25 19:39:11 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 
+class CoordinatesButton extends Teact.Component {
+  handleClick = (event) => {
+    this.props.onReceiveCoordination([event.clientX, event.clientY]);
+  }
+  
+  render() {
+    return (
+      <button onClick={this.handleClick}>Coordinates</button>
+      );
+  }
+}
+
